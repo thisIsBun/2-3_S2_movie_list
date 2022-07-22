7e00b2bd8e2ee82b524c86aa3d84feeb84a96173
Bun-letsgogo <bun.coding@gmail.com>
2022-07-22 23:10:47 +0800

    setup main by handlebars

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const exhbs = require('express-handlebars')
+const app = express()
+const port = 3000
+
+app.engine('handlebars', exhbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.listen(port, () => {
+  console.log(`Express is listening to http://localhost:${port}`)
+})

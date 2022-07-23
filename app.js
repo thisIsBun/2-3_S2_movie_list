e898c0e24f4bc48daf0978e284de881f414c4d45
Bun-letsgogo <bun.coding@gmail.com>
2022-07-23 09:18:18 +0800

    setup index and show page

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const exhbs = require('express-handlebars')
+const movieList = require('./movies.json')
 const app = express()
 const port = 3000
 
@@ -9,7 +10,12 @@ app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', { movies: movieList.results })
+})
+
+app.get('/movies/:movie_id', (req, res) => {
+  const movie = movieList.results.find(movie => movie.id.toString() === req.params.movie_id)
+  res.render('show', {movie})
 })
 
 app.listen(port, () => {
